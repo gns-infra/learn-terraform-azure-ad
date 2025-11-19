37e56dcc1703d4884b1f1b5ae79ffa081bee33dd
gns-infra <geoffreyleop@gmail.com>
2025-11-19 00:31:25 +0000

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.47.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
